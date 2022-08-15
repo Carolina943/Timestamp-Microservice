8092a5bd3cb33413efbc8803c6671f6441e3d09d
Carolina943 <akamifox943@gmail.com>
2022-08-15 11:36:02 -0400

    modified file

--- app.js
+++ app.js
@@ -9,13 +9,23 @@ app.use(cors({optionsSuccessStatus: 200}));
 
 
 app.use(express.static('./public'));
+
 app.get('/', function(req,res){
   res.sendFile(path.resolve(__dirname, '/.public/index.html'))
 })
 
+app.get('/api/:time', function(req, res, next) {
+  req.time = new Date().toString();  
+  next();
+}, function(req, res) {
+  res.json({"unix": req.time});
+});
+
 app.all('*', function(req, res){
     res.status(404).send('resorce not found')
 })
+
+
 
 app.listen(5000, function(){
     console.log('server is listening on port 5000...')
